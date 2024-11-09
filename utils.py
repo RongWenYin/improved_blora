207ad636fa57b932cd91b5c7306f4da2d702d827
yinrong0121 <yinrong0121@gmail.com>
2024-11-09 09:00:15 +0800

    add instantstyle

--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ from huggingface_hub import hf_hub_download
 from PIL import Image 
 import torch  
 import time  
-import transformers  
+import transformers
+from ip_adapter import IPAdapterXL  
 
 # Define blocks to target specific model components for fine-tuning
 BLOCKS_M = {
@@ -17,6 +18,25 @@ BLOCKS_M = {
     'W6': ['unet.up_blocks.0.attentions.2'],
     # Additional layer and block mappings for fine-tuning customization
 }
+
+BLOCKS_INST = {
+    'W0':['down_blocks.1.attentions.1'],
+    'W1':['down_blocks.2.attentions.0'],
+    'W2':['down_blocks.2.attentions.1'],
+    'W3':['mid_block.attentions.0'],
+    'W4':['up_blocks.0.attentions.0'],
+    'W5':['up_blocks.0.attentions.1'],
+    'W6':['up_blocks.0.attentions.2'],
+    'W7':['up_blocks.1.attentions.0'],
+    'W8':['up_blocks.1.attentions.1'],
+    'W9':['up_blocks.1.attentions.2'],
+}
+
+
+ip_ckpt='ipadapter/sdxl_models/ip-adapter_sdxl.bin'
+image_encoder_path='ipadapter/sdxl_models/image_encoder'
+base_model_path = "stabilityai/stable-diffusion-xl-base-1.0"
+device = "cuda"
 
 # Function to check if a layer belongs to specific model blocks
 def is_belong_to_blocks(key, blocks):
@@ -101,7 +121,7 @@ def genImagesBatch(layers, pipeline, objectNames, vae, style_B_LoRA_path,promptK
     # Initialize the pipeline if it is not yet created
     if pipeline is None:
         pipeline = StableDiffusionXLPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-xl-base-1.0",
+            base_model_path,
             vae=vae,
             torch_dtype=torch.float16,
         ).to("cuda")
@@ -121,3 +141,52 @@ def genImagesBatch(layers, pipeline, objectNames, vae, style_B_LoRA_path,promptK
         image.save(f'{styleKey}__{objectName}__{layers}__{itemstep}.png')
         # Clear GPU memory after saving each image
         torch.cuda.empty_cache()
+
+
+def genImgInst(layerList, styleKey, objectNames):
+    refImg="./images/{styleKey}/{styleKey}.png"
+    for layers in layerList:
+        # Split layers into individual block names
+        itemLayerList = layers.split('_')
+        
+        # Collect all the blocks associated with the layers
+        blocks = []
+        for lay in itemLayerList:
+            blocks.extend(BLOCKS_M[lay])
+            
+        # Load the SDXL pipeline with reduced memory consumption
+        pipe = StableDiffusionXLPipeline.from_pretrained(
+            base_model_path,
+            torch_dtype=torch.float16,
+            add_watermarker=False,
+        )
+
+        # Enable VAE tiling for memory optimization
+        pipe.enable_vae_tiling()
+        
+        # Load the IP Adapter model
+        ip_model = IPAdapterXL(pipe, image_encoder_path, ip_ckpt, device, target_blocks=blocks)
+
+        # Open and resize the reference image
+        image = Image.open(refImg).resize((512, 512))
+
+        # Generate images for each object name
+        for objectName in objectNames:
+            # Generate image variations using the image prompt
+            images = ip_model.generate(
+                pil_image=image,
+                prompt=f'a {objectName}, masterpiece, best quality, high quality',
+                negative_prompt="text, watermark, lowres, low quality, worst quality, deformed, glitch, low contrast, noisy, saturation, blurry",
+                scale=1.0,
+                guidance_scale=5,
+                num_samples=1,
+                num_inference_steps=30,
+                seed=42,
+            )
+            
+            # Save the generated image with a descriptive filename
+            print(f'Done processing layer: {layers}')
+            images[0].save(f'inst__{styleKey}__{objectName}__{layers}__1000.png')
+            
+            # Free up memory by clearing the pipeline cache
+            freeCache(pipe)
